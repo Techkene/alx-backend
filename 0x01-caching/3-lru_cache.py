f6d1b040f3a5e03b5c2bd4ba03bae522ee756f44
Techkene <keneanoliefo24@gmail.com>
2024-02-02 02:22:04 +0100

    update

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -17,8 +17,11 @@ you must discard the least recently used item
 (LRU algorithm)
 you must print DISCARD: with the key discarded
 and following by a new line
- """
-
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
+"""
 
 
 BaseCaching = __import__('base_caching').BaseCaching
